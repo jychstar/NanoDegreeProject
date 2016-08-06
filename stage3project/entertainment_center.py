72c2d376972b784cecb66a57aa05e0465d19a33f
Yuchao Jiang <jychstar@gmail.com>
2016-08-06 10:24:00 -0500

    resubmit stage 3 project after code review 1

--- stage3project/entertainment_center.py
+++ stage3project/entertainment_center.py
@@ -1,11 +1,18 @@
 import fresh_tomatoes
 import media
-zootopia=media.Movie("Zootopia","Animals fight for equal rights","https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg","https://www.youtube.com/watch?v=WWFB-zrxn7o")
+zootopia=media.Movie("Zootopia",
+								"Animals fight for equal rights",
+								"https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
+								"https://www.youtube.com/watch?v=WWFB-zrxn7o")
+								# the prefix https:// can't be omitted, otherwise cause errors
 
-terminator_2=media.Movie("Terminator 2: Judgment Day", "Future robots time travel to change the world", "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg", "https://www.youtube.com/watch?v=PZY-5SmjvXg")
-
-#print zootopia.storyline
-#terminator_2.show_trailer()
-
-movies=[zootopia, terminator_2]
-fresh_tomatoes.open_movies_page(movies)
+terminator_2=media.Movie("Terminator 2: Judgment Day",
+									"Future robots time travel to change the world", "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg", "https://www.youtube.com/watch?v=PZY-5SmjvXg")
+amelie = media.Movie("Amelie",
+                     "A fanciful comedy about a young woman who discretely \
+                         orchestrates the lives of the people around her, \
+                         creating a world exclusively of her own making.",
+                     "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",
+                     "https://www.youtube.com/watch?v=6Q537310azE")
+movies=[zootopia, terminator_2, amelie]  # create a list of moives
+fresh_tomatoes.open_movies_page(movies) # fresh_tomatoes module create a static page to display movie
--- stage3project/media.py
+++ stage3project/media.py
@@ -1,10 +1,13 @@
 import webbrowser
 class Movie():
+	""" a class to store the structured data of movie"""
 	def __init__(self, movie_title,movie_storyline,poster_image_url, trailer_youtube_url):
+		""" define constructor, which takes 4 inputs """
 		self.title=movie_title
 		self.storyline=movie_storyline
 		self.poster_image_url=poster_image_url
 		self.trailer_youtube_url=trailer_youtube_url
 
 	def show_trailer(self):
+		""" open trailer link using webbrowser module """
 		webbrowser.open(self.trailer_youtube_url)
